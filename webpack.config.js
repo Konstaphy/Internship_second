82eae507510c948ba2fe105b0295b9f873975344
Konstaphy <Konstaphy@gmail.com>
2021-09-07 19:53:53 +0500

    Added webpack + headers layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path')
+const miniCss = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: {main: './start.js'},
+    entry: {main: './src/start.js'},
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    mode: 'development'
+    mode: 'development',
+    module: {
+        rules: [{
+            test:/\.(s*)css$/,
+            use: [
+                miniCss.loader,
+                'css-loader',
+                'sass-loader',
+            ]
+        }]
+    },
+    plugins: [
+        new miniCss({
+            filename: 'style.css',
+        }),
+    ]
 }
