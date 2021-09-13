a1085db4a5eabaf4a9b015425cbe5f1fca586ab8
Konstaphy <Konstaphy@gmail.com>
2021-09-13 21:03:59 +0500

    repaired some stuff

--- src/start.js
+++ src/start.js
@@ -1,3 +1,3 @@
 import './styles.scss'
 
-console.log("Hello")
+console.log("HELLO")
--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,23 @@ const path = require('path')
 const htmlWPP = require('html-webpack-plugin')
 
 module.exports = {
-    entry: {
-        main: './src/start.js'
-    },
+    entry: './src/start.js'
+    ,
     output: {
         filename: "index.[contenthash].js",
         path: path.resolve(__dirname, 'build')
     },
-    mode: 'production',
+    mode: "production",
     module: {
         rules: [
         {
-        test:/\.scss$/,
-        use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader',
-        ]
-    }, //scss
+            test:/\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader',
+            ]
+        }, //scss
         {
             test: /\.pug$/,
             use: [
@@ -32,18 +31,14 @@ module.exports = {
     plugins: [
         new htmlWPP({
             filename: "index.html",
-            template: path.join(__dirname, 'public/app.pug'),
-        })
+            template: path.join(__dirname, 'public/app.pug')
+        }),
     ],
     devServer: {
         static: {
             directory: path.join(__dirname, 'build'),
         },
-        compress: true,
-        port: 9000,
-    },
-    performance: {
-        hints: false,
+        port: 3000,
     },
     stats: 'errors-only',
 }
