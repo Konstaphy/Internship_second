4ef2695b772673b3ac692fb26cf6e8af73ca3393
Konstaphy <konstafyf@gmail.com>
2021-09-23 21:07:22 +0500

    Refactored button, repaired all linkings

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,3 +1,2 @@
 import './header.scss'
 import './header__registered/header__registered.scss'
-import './header__unregistered/header__unregistered.scss'
--- src/start.js
+++ src/start.js
@@ -5,6 +5,7 @@ import 'material-icons/iconfont/material-icons.css';
 //js
 // components
 import './elements/dropdown/dropdown'
+import './elements/button/button'
 import './components/header/header'
 import './components/footer/footer'
 import './components/auth/login/login'
