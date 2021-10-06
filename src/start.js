493decc27270459c7106be3c06489889f4aa379f
Konstaphy <konstafyf@gmail.com>
2021-10-06 21:32:18 +0500

    checkboxes oop realisation

--- src/start.js
+++ src/start.js
@@ -1,12 +1,16 @@
 //styles
 import './styles.scss'
 import 'material-icons/iconfont/material-icons.css';
+import Checkbox from "./elements/checkbox-element/checkbox";
+
 
 //js
 // components
 function requireAll(requireContext) {
     return requireContext.keys().map(requireContext);
 }
+
+Checkbox.init('ce1', 'ce2')
 
 requireAll(require.context('./elements', true, /\.js$/));
 requireAll(require.context('./components', true, /\.js$/));
