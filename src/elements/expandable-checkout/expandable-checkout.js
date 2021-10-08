d3da69ce4adc32b05f0fbf5949529f315b79c3d9
Konstaphy <konstafyf@gmail.com>
2021-10-08 17:25:46 +0500

    expandable checklist refactored

--- src/elements/expandable-checkout/expandable-checkout.js
+++ src/elements/expandable-checkout/expandable-checkout.js
@@ -2,6 +2,6 @@ const checkboxes = document.getElementsByClassName('checkbox-container-title')
 
 for (let i = 0; i < checkboxes.length; i++){
     checkboxes[i].addEventListener('click', () => {
-        checkboxes[i].classList.toggle('closed')
+        checkboxes[i].parentElement.classList.toggle('closed')
     })
 }
